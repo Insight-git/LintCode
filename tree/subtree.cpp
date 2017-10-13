10d751074d053dbb4b39af140e436f82bff93d4e
Insight-git <ljw0608.mail@gmail.com>
2017-10-13 11:23:19 +0800

    a

--- tree/subtree.cpp
+++ tree/subtree.cpp
@@ -19,35 +19,40 @@ public:
      * @param T2: The roots of binary tree T2.
      * @return: True if T2 is a subtree of T1, or false.
      */
-    void PreOrderTraverse(TreeNode *T1, TreeNode *T2, bool &result)
-    {
-        if(!T1&&!T2)
-        	return;
-        if((!T1&&T2)||(!T2&&T1))
-        {
-        	result = false;
-        	return;
-        }
+	bool IsIdentical(TreeNode *T1, TreeNode *T2)
+	{
+		static bool result = true;
+		if(!T1&&!T2)
+			return result;
+		if((!T1&&T2)||(!T2&&T1))
+		{
+			result = false;
+			return result;
+		}
+		if(T1->val!=T2->val)
+		{
+			result = false;
+			return result;
+		}
+		IsIdentical(T1->left,T2->left);
+		IsIdentical(T1->right,T2->right);
+		return result;
+	}
+    bool isSubtree(TreeNode * T1, TreeNode * T2) {
+        bool result = false;
+        if(!T2)
+            return true;
         if(T1->val==T2->val)
         {
-        	result = true;
-        	PreOrderTraverse(T1->left,T2->left,result);
-        	PreOrderTraverse(T1->right,T2->right,result);
-        	return;
+        	result = IsIdentical(T1,T2);
+        	if(result)
+        		return result;
         }
         else
         {
-        	result = false;
-        	PreOrderTraverse(T1->left,T2,result);
-        	PreOrderTraverse(T1->right,T2,result);
-        	return;
+        	isSubtree(T1->left, T2);
+        	isSubtree(T1->right, T2);
         }
-    }
-    bool isSubtree(TreeNode * T1, TreeNode * T2) {
-        bool result = true;
-        if(!T2)
-            return true;
-        PreOrderTraverse(T1,T2,result);
         return result;
     }
 };
